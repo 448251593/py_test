0b71de155ba4b5b0131d7ef83590ae459d5d6d0c
448251593 <448251593@qq.com>
2017-12-27 23:52:41 -0800

    auto mv blog_data

--- selenium/caijiqqlog.py
+++ selenium/caijiqqlog.py
@@ -33,10 +33,10 @@ def get_page_links(page_num):
 	driver.find_element_by_xpath("//button[@type='button']").click()
 	print ('4')
 	print('sleep 5')
-	time.sleep(5);
+	driver.implicitly_wait(5);
 	
-	with open("page_source_frame_new.html",'wb') as f:
-		f.write(driver.page_source);
+	#with open("source_frame_new.html",'wb') as f:
+	#	f.write(driver.page_source);
 	try:
 		Links = driver.find_elements_by_xpath("//a[starts-with(@href,'http://user.qzone.qq.com/822989010/blog/')]");
 		count1 = 0;
@@ -45,15 +45,27 @@ def get_page_links(page_num):
 			url_context= link.get_attribute('href');
 			print (url_context);
 			total=total+1;
-
+			#str_title = link.get_attribute('innerHTML');
+			#str_title = str_title.strip();
+			#str_title = str_title.replace('<span>','');
+			#str_title = str_title.replace('</span>','');
+			#print(str_title);
 		for link in Links:
 			url_context= link.get_attribute('href');
-
+			
 			count1=count1+1
 			print('progress=('+str(count1)+'/'+str(total)+')')
-			rslt = get_log_context(driver1,url_context);
+			
+			str_title = link.get_attribute('innerHTML');
+			str_title = str_title.strip();
+			str_title = str_title.replace('<span>','');
+			str_title = str_title.replace('</span>','');
+			
+			rslt = get_log_context(driver1,url_context,str_title);
 			if rslt == -1:
-				err_log('get_log_context err, url='+url_context)
+				err_log('err,url='+url_context);
+				pathfile = create_path_base_url(url);
+				os.system("rm "+pathfile+' -rf');
 	except:
 		print('find links err');
 
@@ -69,15 +81,17 @@ def driver_browser_init():
 		rstl = -1;
 		
 	try:
+		
 		driver.switch_to.frame(0);		
 		print('switch frame 0 ok,sleep 10');
-		time.sleep(10);		
+		#time.sleep(10);
+		driver.implicitly_wait(10);		
 	except:
 		print('switch frame 0 err');
 		rstl = -1;
 
-	with open("page_source_frame.html",'wb') as f:
-		f.write(driver.page_source);
+	#with open("source_frame.html",'wb') as f:
+	#	f.write(driver.page_source);
 	return 0
 def create_id():
     m = hashlib.md5(str(time.clock()).encode('utf-8'))
@@ -90,7 +104,7 @@ def getHtml(url):
 	content = urllib2.urlopen(req).read()
 	return content
 	
-def get_log_context(driver_web,url):
+def get_log_context(driver_web,url,str_title):
 	print ('getting  ' + url)
 	pathfile = create_path_base_url(url)
 	if len(pathfile)==0:
@@ -119,7 +133,7 @@ def get_log_context(driver_web,url):
 		return -1;
 
 
-	#with open("page_source_1.txt",'wb') as f:
+	#with open("source_1.txt",'wb') as f:
 	#	f.write(driver_web.page_source)
 	
 	try:
@@ -130,19 +144,19 @@ def get_log_context(driver_web,url):
 		print("switch_to.frame err")
 		return -1;
 
-	save_filename = '' 
-	try:
-		#elem = driver_web.find_elements_by_xpath("//*[@class=blog_tit_detail]")
-		elem = driver_web.find_element_by_class_name("blog_tit_detail")
-		save_filename = elem.get_attribute('textContent')
-		save_filename = save_filename.strip()
-		print('title='+save_filename)
-	except:
-		print('find_elements_by_xpath blog_tit_detail err')
-		return -1;
+	save_filename = str_title 
+	#try:
+	#	#elem = driver_web.find_elements_by_xpath("//*[@class=blog_tit_detail]")
+	#	elem = driver_web.find_element_by_class_name("blog_tit_detail")
+	#	save_filename = elem.get_attribute('textContent')
+	#	save_filename = save_filename.strip()
+	#	print('title='+save_filename)
+	#except:
+	#	print('find_elements_by_xpath blog_tit_detail err')
+	#	return -1;
 
 		
-	#with open("page_source_2.txt",'wb') as f:
+	#with open("source_2.txt",'wb') as f:
 	#	f.write(driver_web.page_source)
 
 	
@@ -169,9 +183,9 @@ def get_log_context(driver_web,url):
 		print('find_element_by_id ok')	
 		with open(pathfile+"/"+save_filename+".html",'wb') as f:
 			f.write(str_txt)
-		print(pathfile+"/page_source.html"+'save ok')
+		print(pathfile+" /page_source.html "+'save ok')
 	except:
-		print ("loading pic err")		
+		print ("loading pic err")
 		return -1;
 	return 0;
 	
@@ -196,9 +210,10 @@ def create_path_base_url(url):
 	strid = re.findall(r'/blog/[0-9]*',url)
 	path = strid[0][1:len(strid[0])]
 	return path
-	
+
+file_errlog="err_log_caijiqqlog.log";
 def err_log(log_data):
-	with open("err_log_caijiqqlog.log",'a') as f:
+	with open(file_errlog,'a') as f:
 		f.write(log_data+'\n')	
 	
 def debug_test():
@@ -235,9 +250,12 @@ if __name__ == '__main__':
 	try:
 		rslt = driver_browser_init();
 		if rslt == 0:
-			page_num = 4
-			get_page_links(str(page_num));
-			os.system("mv blog page"+str(page_num))
+			for page_num in range(15,16):
+				#page_num = 8
+				get_page_links(str(page_num));
+				os.system("mv blog page"+str(page_num));
+				os.system('mv '+file_errlog+' page'+str(page_num));					
+			os.system("mv  page* blog_data");	
 		else:
 			print('driver_browser_init');
 	except:
---
+++ while.py
@@ -0,0 +1,9 @@
+import os;
+
+def test():
+	for num in range(10,20):
+		print(num);
+
+
+if __name__ == '__main__':
+	test();
