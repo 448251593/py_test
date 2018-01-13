0df83a46b3f46a4bd568733f141279a1c7d48c25
毕晨光 <448251593@qq.com>
2018-01-13 18:48:47 +0800

    hao123 get links debug

--- hao123/baiduhao123_caiji.py
+++ hao123/baiduhao123_caiji.py
@@ -25,89 +25,87 @@ driver1.minimize_window();
 reload(sys)
 sys.setdefaultencoding( "utf-8" );
 
-def get_page_links(page_num):
-	try:
-		if page_num!='1':
-			print('get page='+page_num+' data');
-			driver.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").click()
-			print ('1')
-			driver.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").clear()
-			print ('2')
-			driver.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").send_keys(page_num)
-			print ('3')
-			driver.find_element_by_xpath("//button[@type='button']").click()
-			print ('4')
-			print('sleep 5')
-	except:
-		print('go to page err');
-		return;
-	time.sleep(3);
+def get_page_links(driverweb,page_num):
+	#try:
+	#	if page_num!='1':
+	#		print('get page='+page_num+' data');
+	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").click()
+	#		print ('1')
+	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").clear()
+	#		print ('2')
+	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").send_keys(page_num)
+	#		print ('3')
+	#		driverweb.find_element_by_xpath("//button[@type='button']").click()
+	#		print ('4')
+	#		print('sleep 5')
+	#except:
+	#	print('go to page err');
+	#	return;
+	#time.sleep(3);
 	
-	#with open("source_frame_new.html",'wb') as f:
-	#	f.write(driver.page_source);
-
+	div_str = '';
 	try:
-		Links = driver.find_elements_by_xpath("//a[starts-with(@href,'http://user.qzone.qq.com/822989010/blog/')]");
-		count1 = 0;
-		total = 0;
-		for link in Links:
-			url_context= link.get_attribute('href');
-			print (url_context);
-			total=total+1;
+		div_str = driver_web.find_element_by_id('J_mainList').get_attribute('innerHTML')
+	except:
+		print('find J_mainList err');
+	
+	with open("J_mainList.html",'wb') as f:
+		f.write(driverweb.page_source);	
+	
+	print(driver_web.find_element_by_id('list clearfix'))
+	try:
+		print('nop');
+		#Links = driverweb.find_elements_by_xpath("//a[starts-with(@href,'http://user.qzone.qq.com/822989010/blog/')]");
+		#count1 = 0;
+		#total = 0;
+		#for link in Links:
+		#	url_context= link.get_attribute('href');
+		#	print (url_context);
+		#	total=total+1;
 			#str_title = link.get_attribute('innerHTML');
 			#str_title = str_title.strip();
 			#str_title = str_title.replace('<span>','');
 			#str_title = str_title.replace('</span>','');
 			#print(str_title);
-		for link in Links:
-			url_context= link.get_attribute('href');
-			
-			count1=count1+1
-			print('progress=('+str(count1)+'/'+str(total)+')')
-			
-			str_title = link.get_attribute('innerHTML');
-			str_title = str_title.strip();
-			str_title = str_title.replace('<span>','');
-			str_title = str_title.replace('</span>','');
-			
-			#pathfile = create_path_base_url(url);
-			try:
-				rslt = get_log_context(driver1,url_context,str_title,page_num);
-				if rslt == -1:
-					err_log('page='+page_num+','+url_context);					
-					os.system("rm "+pathfile_current+' -rf');
-			except:
-				err_log('page='+page_num+','+url_context);
-				os.system("rm "+pathfile_current+' -rf');
+		#for link in Links:
+		#	url_context= link.get_attribute('href');
+		#	
+		#	count1=count1+1
+		#	print('progress=('+str(count1)+'/'+str(total)+')')
+		#	
+		#	str_title = link.get_attribute('innerHTML');
+		#	str_title = str_title.strip();
+		#	str_title = str_title.replace('<span>','');
+		#	str_title = str_title.replace('</span>','');
+		#	
+		#	#pathfile = create_path_base_url(url);
+		#	try:
+		#		rslt = get_log_context(driver1,url_context,str_title,page_num);
+		#		if rslt == -1:
+		#			err_log('page='+page_num+','+url_context);					
+		#			os.system("rm "+pathfile_current+' -rf');
+		#	except:
+		#		err_log('page='+page_num+','+url_context);
+		#		os.system("rm "+pathfile_current+' -rf');
 
 
 	except:
 		print('find links err');
 
-def driver_browser_init():
+def driver_browser_init(driverweb):
 	rstl = 0
-	url = 'https://user.qzone.qq.com/822989010/2';
+	url = 'http://www.hao123.com/gaoxiao/haomeizi/list';
 	#url = 'file:///home/bcg/samba/test/selenium/page_source_frame.html'
 	try:
-		driver.get(url);	
+		driverweb.get(url);	
 		print("get  "+url+'  ok' );
 	except:
 		print('get url err');
 		rstl = -1;
-		
-	try:
-		
-		driver.switch_to.frame(0);		
-		print('switch frame 0 ok,sleep 10');
-		time.sleep(10);
-		#driver.implicitly_wait(10);		
 
-	except:
-		print('switch frame 0 err');
-		rstl = -1;
-
+	time.sleep(10);
 	#with open("source_frame.html",'wb') as f:
-	#	f.write(driver.page_source);
+	#	f.write(driverweb.page_source);
 	return 0;
 
 def create_id():
@@ -284,22 +282,22 @@ def debug_test():
 			
 if __name__ == '__main__':
 	
-	#try:
-	#	rslt = driver_browser_init();
-	#	if rslt == 0:
-	#		#for page_num in range(25,27):				
-	#		page_num = 141;
-	#		get_page_links(str(page_num));
-	#		page_num = 142;
-	#		get_page_links(str(page_num));
-	#		
-	#		#os.system("mv  page* blog_data");	
-    #
-	#	else:
-	#		print('driver_browser_init');
-	#except:
-	#	print ('driver_browser_init err');
-	get_log_context(driver1,'http://www.hao123.com/gaoxiao/haomeizi/1110','','')
+	try:
+		rslt = driver_browser_init(driver1);
+		if rslt == 0:
+			#for page_num in range(25,27):				
+			page_num = 141;
+			get_page_links(driver1,str(page_num));
+			#page_num = 142;
+			#get_page_links(str(page_num));
+			
+			#os.system("mv  page* blog_data");	
+    
+		else:
+			print('driver_browser_init');
+	except:
+		print ('driver_browser_init err');
+	#get_log_context(driver1,'http://www.hao123.com/gaoxiao/haomeizi/1110','','')
 	#debug_test()	
 
 	driver1.quit();
