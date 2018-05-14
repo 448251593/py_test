794844452ff6dfd6c3da0e6e913fbd94a85452e1
448251593 <448251593@qq.com>
2018-05-15 07:02:56 +0800

    hao123gaoxiao 优化

--- hao123gaoxiao/hao123gaoxiao_caiji.py
+++ hao123gaoxiao/hao123gaoxiao_caiji.py
@@ -40,117 +40,19 @@ def sqlite3_con():
 	
 conn=sqlite3_con();
 
-def get_page_links(driverweb,page_num):
-	#try:
-	#	if page_num!='1':
-	#		print('get page='+page_num+' data');
-	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").click()
-	#		print ('1')
-	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").clear()
-	#		print ('2')
-	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").send_keys(page_num)
-	#		print ('3')
-	#		driverweb.find_element_by_xpath("//button[@type='button']").click()
-	#		print ('4')
-	#		print('sleep 5')
-	#except:
-	#	print('go to page err');
-	#	return;
-	#time.sleep(3);
-	
-	div_str = '';
-	try:
-		div_str = driverweb.find_element_by_id('J_mainList').get_attribute('innerHTML')
-		#div_str = driverweb.find_element_by_id('J_mainList').get_attribute('innerHTML')
-	except:
-		print('find J_mainList err');
-	
-	#with open("J_mainList.html",'wb') as f:
-	#	f.write(driverweb.page_source);	
-	
-	#print(div_str);
-	try:
-		
-		
-		str_txt = re.compile(r'<img.+?>').sub('', div_str);
-		#print(str_txt);
-		urlarr = re.findall(r'<a.*?>.*?</a>',str_txt);
-		for  elem in urlarr:
-			if elem.find("class=\"title\"") != -1:
-				href_url_arr= re.findall(r'href=\".*?\"',elem); 
-				href_url = href_url_arr[0][6:len(href_url_arr[0])-1];
-				print(href_url);
-				href_title_arr =  re.findall(u'>.*?<', elem);
-				href_title =  href_title_arr[0][1:len(href_title_arr[0])-1];
-				print(href_title);
-				
-				try:
-					get_log_context(driver1,href_url);
-				except:
-					print('getlog err');
-		#Links = driverweb.find_elements_by_xpath("//a[starts-with(@href,'http://user.qzone.qq.com/822989010/blog/')]");
-		#count1 = 0;
-		#total = 0;
-		#for link in Links:
-		#	url_context= link.get_attribute('href');
-		#	print (url_context);
-		#	total=total+1;
-			#str_title = link.get_attribute('innerHTML');
-			#str_title = str_title.strip();
-			#str_title = str_title.replace('<span>','');
-			#str_title = str_title.replace('</span>','');
-			#print(str_title);
-		#for link in Links:
-		#	url_context= link.get_attribute('href');
-		#	
-		#	count1=count1+1
-		#	print('progress=('+str(count1)+'/'+str(total)+')')
-		#	
-		#	str_title = link.get_attribute('innerHTML');
-		#	str_title = str_title.strip();
-		#	str_title = str_title.replace('<span>','');
-		#	str_title = str_title.replace('</span>','');
-		#	
-		#	#pathfile = create_path_base_url(url);
-		#	try:
-		#		rslt = get_log_context(driver1,url_context,str_title,page_num);
-		#		if rslt == -1:
-		#			err_log('page='+page_num+','+url_context);					
-		#			os.system("rm "+pathfile_current+' -rf');
-		#	except:
-		#		err_log('page='+page_num+','+url_context);
-		#		os.system("rm "+pathfile_current+' -rf');
 
-
-	except:
-		print('find links err');
-
-def driver_browser_init(driverweb):
-	rstl = 0
-	#url = 'http://www.hao123.com/gaoxiao/haomeizi/list';
-	url = 'file:///D:/py_test/hao123/J_mainList.html'
-	try:
-		driverweb.get(url);	
-		print("get  "+url+'  ok' );
-	except:
-		print('get url err');
-		rstl = -1;
-
-	time.sleep(10);
-	#with open("source_frame.html",'wb') as f:
-	#	f.write(driverweb.page_source);
-	return 0;
 
 def create_id():
     m = hashlib.md5(str(time.clock()).encode('utf-8'))
     return m.hexdigest()	
-#通过url获取网页
+#通过url获取图片
 def getHtml(url):
 	#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
 	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}			
 	req = urllib2.Request(url = url, headers = headers)
 	content = urllib2.urlopen(req).read()
 	return content
+	
 pathfile = "";
 def init_get(driver_web,url):
 	print ('getting  ' + url)
@@ -178,21 +80,12 @@ def init_get(driver_web,url):
 	except:
 		print ("find J_article err")
 		return -1;
-		
+#获取网页内容并提取图片保存		
 def get_log_context(driver_web,count):
 
 	#
 	with open("current_url.txt",'wb') as f:
 		f.write(driver_web.current_url);
-		
-	#save_filename = driver_web.title;
-	#save_filename= re.compile(r' ').sub('', save_filename);
-	#save_filename= re.compile(r'#').sub('', save_filename);
-	#save_filename= save_filename.replace('hao123');
-	#save_filename= save_filename.replace('上网导航');
-	
-	
-
 	try:
 		div_str = driver_web.find_element_by_class_name('brief').get_attribute('innerHTML')
 		#print ("find brief ok "+div_str);
@@ -232,6 +125,7 @@ def get_log_context(driver_web,count):
 		for  elem in urlarr:	
 			picurl_str		= urlarr[0][len('src="'):len(urlarr[0])-2];
 			print("pic----"+picurl_str)
+			#获取保存图片
 			get_file_and_save(picurl_str,'hao123gaoxiao/', count, save_filename);		#save_filename[0:len(save_filename)-12]
 			return 2;
 			break;
@@ -240,6 +134,7 @@ def get_log_context(driver_web,count):
 		print("pic url find err");
 		
 	return 0;
+#点击下一页函数
 def  get_next_click(driver_web,count):
 	try:
 		print("next click ok ct="+str(count));
@@ -284,9 +179,10 @@ file_errlog="hao123_caijilog.log";
 def err_log(log_data):
 	with open('haomeizi/'+file_errlog,'a') as f:
 		f.write(log_data+'\n')	
+#运行主函数
 def exe_main(count):
 
-	
+	#浏览器操作变量
 	driver1=webdriver.Firefox();
 	driver1.minimize_window();	
 	with open("current_url.txt",'r') as f:
@@ -300,10 +196,11 @@ def exe_main(count):
 		
 		while(tmp_ct < count):
 			tmp_ct = tmp_ct + 1;
-			ret2 = get_log_context(driver1,tmp_ct);
+			ret2 = get_log_context(driver1,tmp_ct);#获取当前网页的图片
 			if ret2 == 2:
-				time.sleep(9);
-				get_next_click(driver1,tmp_ct);
+				time.sleep(3);
+				get_next_click(driver1,tmp_ct);#点击下一页
+				time.sleep(6);#点击完成延时等待网页
 			else:
 				print("err break=" + str(ret2));
 				#break;
