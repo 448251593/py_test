43af96552c5403a09933920432b0f8e1946bffb0
448251593 <448251593@qq.com>
2018-06-29 06:50:25 +0800

    caicai weixin youhua

--- caicai/caicai_caiji.py
+++ caicai/caicai_caiji.py
@@ -9,39 +9,10 @@ import imghdr;
 #使用selenium的隐藏PhantimJS浏览器登陆账号后对内容获�?#注意frame与iframe的格式框切换
 #driver = webdriver.PhantomJS(executable_path="E:\\mac\\id\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe")
 #driver=webdriver.Chrome()
-
-#driver=webdriver.PhantomJS()
-#driver.set_preference('network.proxy.type', 1)
-#driver.set_preference('network.proxy.http', '127.0.0.1')
-#driver.set_preference('network.proxy.http_port', 17890)
-#driver.maximize_window() 
-
-
-driver1=webdriver.Firefox();
-driver1.minimize_window();
-
-#driver=webdriver.Firefox()
-#driver.minimize_window() 
-reload(sys)
+reload(sys);
 sys.setdefaultencoding( "utf-8" );
 
 def get_page_links(driverweb,page_num):
-	#try:
-	#	if page_num!='1':
-	#		print('get page='+page_num+' data');
-	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").click()
-	#		print ('1')
-	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").clear()
-	#		print ('2')
-	#		driverweb.find_element_by_xpath("//input[starts-with(@id,'pageIndex_input')]").send_keys(page_num)
-	#		print ('3')
-	#		driverweb.find_element_by_xpath("//button[@type='button']").click()
-	#		print ('4')
-	#		print('sleep 5')
-	#except:
-	#	print('go to page err');
-	#	return;
-	#time.sleep(3);
 	
 	div_str = '';
 	try:
@@ -73,58 +44,10 @@ def get_page_links(driverweb,page_num):
 					get_log_context(driver1,href_url);
 				except:
 					print('getlog err');
-		#Links = driverweb.find_elements_by_xpath("//a[starts-with(@href,'http://user.qzone.qq.com/822989010/blog/')]");
-		#count1 = 0;
-		#total = 0;
-		#for link in Links:
-		#	url_context= link.get_attribute('href');
-		#	print (url_context);
-		#	total=total+1;
-			#str_title = link.get_attribute('innerHTML');
-			#str_title = str_title.strip();
-			#str_title = str_title.replace('<span>','');
-			#str_title = str_title.replace('</span>','');
-			#print(str_title);
-		#for link in Links:
-		#	url_context= link.get_attribute('href');
-		#	
-		#	count1=count1+1
-		#	print('progress=('+str(count1)+'/'+str(total)+')')
-		#	
-		#	str_title = link.get_attribute('innerHTML');
-		#	str_title = str_title.strip();
-		#	str_title = str_title.replace('<span>','');
-		#	str_title = str_title.replace('</span>','');
-		#	
-		#	#pathfile = create_path_base_url(url);
-		#	try:
-		#		rslt = get_log_context(driver1,url_context,str_title,page_num);
-		#		if rslt == -1:
-		#			err_log('page='+page_num+','+url_context);					
-		#			os.system("rm "+pathfile_current+' -rf');
-		#	except:
-		#		err_log('page='+page_num+','+url_context);
-		#		os.system("rm "+pathfile_current+' -rf');
-
 
 	except:
 		print('find links err');
 
-def driver_browser_init(driverweb):
-	rstl = 0
-	#url = 'http://www.hao123.com/gaoxiao/haomeizi/list';
-	url = 'file:///D:/py_test/hao123/J_mainList.html'
-	try:
-		driverweb.get(url);	
-		print("get  "+url+'  ok' );
-	except:
-		print('get url err');
-		rstl = -1;
-
-	time.sleep(10);
-	#with open("source_frame.html",'wb') as f:
-	#	f.write(driverweb.page_source);
-	return 0;
 
 def create_id():
     m = hashlib.md5(str(time.clock()).encode('utf-8'))
@@ -141,11 +64,8 @@ pathfile_current='';
 def get_log_context(driver_web,url):
 
 	print ('getting  ' + url)
-	pathfile = create_path_base_url(url)
-	if len(pathfile)==0:
-		print("get path from url err")
-		return 0;
-	pathfile = 'caicai_weixin/'+pathfile	
+
+	pathfile = 'caicai_weixin/'+create_id();	
 	print('pathfile='+pathfile)
 	pathfile_current = pathfile;
 	isExists=os.path.exists(pathfile)
@@ -160,7 +80,6 @@ def get_log_context(driver_web,url):
 	try:
 		driver_web.get(url)
 		#print (driver_web.page_source)		
-		
 		elem = driver_web.find_elements_by_id("img-content")
 		print("find img-content")
 		
@@ -186,19 +105,6 @@ def get_log_context(driver_web,url):
 	save_filename= re.compile(r' ').sub('', save_filename);
 	save_filename= re.compile(r'#').sub('', save_filename);
 	
-	#try:
-	#	#elem = driver_web.find_elements_by_xpath("//*[@class=blog_tit_detail]")
-	#	elem = driver_web.find_element_by_class_name("blog_tit_detail")
-	#	save_filename = elem.get_attribute('textContent')
-	#	save_filename = save_filename.strip()
-	#	print('title='+save_filename)
-	#except:
-	#	print('find_elements_by_xpath blog_tit_detail err')
-	#	return -1;
-
-		
-	#with open("source_2.txt",'wb') as f:
-	#	f.write(driver_web.page_source)
 
 	div_str = ''
 	try:
@@ -209,9 +115,6 @@ def get_log_context(driver_web,url):
 		
 	with open("js_content.txt",'wb') as f:
 		f.write(div_str);
-	
-	
-	
 	try:
 		str_txt=div_str
 		urlarr = re.findall(r'<img\b.*?data-src=\".*?>',str_txt)
@@ -248,7 +151,7 @@ def get_log_context(driver_web,url):
 	return 0;
 	
 	
-	
+#保存图片
 def get_file_and_save(url, path,sortid):
 	cat_img = getHtml(url)
 	print ("down load ok")
@@ -263,70 +166,18 @@ def get_file_and_save(url, path,sortid):
 		f.write(cat_img)
 		print("save pic ok, "+pathfilename)	
 	return filename
-	
-	
 
-def create_path_base_url(url):
-	#strid = re.findall(r'/haomeizi/[0-9]*',url)#haomeizi/1109
-	#path = strid[0][10:len(strid[0])]
-	r_index = url.rindex('/')+1;
-	return url[r_index:len(url)];
-
-file_errlog="hao123_caijilog.log";
-def err_log(log_data):
-	with open('haomeizi/'+file_errlog,'a') as f:
-		f.write(log_data+'\n')	
 	
-def debug_test():
-	pathfile = create_path_base_url('https://user.qzone.qq.com/822989010/blog/1488525103')
-	if len(pathfile)==0:
-		print("get path from url err")
-		return
-	pathfile = './'+pathfile	
-	print('pathfile='+pathfile)
-	
-	
-	with open("page_source_2.html",'r') as f:
-		str_txt=f.read()
-		#urlarr = re.findall(r'data-src=".*?"',str_txt)
-		urlarr = re.findall(r'<img\b.*?data-src=".*?>',str_txt)
-		for  elem in urlarr:			
-			tmp_url = elem[len('data-src=')+1:len(elem)-1]
-			#file_path_url = get_file_and_save(tmp_url,pathfile)
-			#print(file_path_url)
-			#str_txt = str_txt.replace(tmp_url,file_path_url)
-			print(tmp_url)
-			
-		try:
-			with open(pathfile+"/page_source_31.html",'wb') as f:
-				f.write(str_txt)
-		except:
-			print('save err')
-	
-			
-			
 if __name__ == '__main__':
-	
-	#try:
-	#	rslt = driver_browser_init(driver1);
-	#	if rslt == 0:
-	#		#for page_num in range(25,27):				
-	#		page_num = 141;
-	#		get_page_links(driver1,str(page_num));
-	#		#page_num = 142;
-	#		#get_page_links(str(page_num));
-	#		
-	#		#os.system("mv  page* blog_data");	
-    #
-	#	else:
-	#		print('driver_browser_init');
-	#except:
-	#	print ('driver_browser_init err');
-	get_log_context(driver1,'https://mp.weixin.qq.com/s/3zou7JIzBUx7wtFAg8KYsw');
-	#get_log_context(driver1,'');
-	
+		
+	driver1=webdriver.Firefox();
+	driver1.minimize_window();
+    #把url整理好按照下面的方式复制多行修改
+	get_log_context(driver1,'http://mp.weixin.qq.com/s?__biz=MzA4OTIzMjYzMg==&mid=2652541940&idx=1&sn=15e84a6cbd551375ac7d27fe95010da3&scene=19#wechat_redirect');
+	#get_log_context(driver1,'http://mp.weixin.qq.com/s?__biz=MzA4OTIzMjYzMg==&mid=2652541582&idx=1&sn=17c3e94d2956760f17a6862ede2dc5c6&scene=19#wechat_redirect');
+	#get_log_context(driver1,'http://mp.weixin.qq.com/s?__biz=MzA4OTIzMjYzMg==&mid=2652541582&idx=2&sn=a4ecf38a29040c35a56b72f782ac3aac&scene=19#wechat_redirect');
+	#get_log_context(driver1,'http://mp.weixin.qq.com/s?__biz=MzA4OTIzMjYzMg==&mid=2652541582&idx=3&sn=9259f0eb146fcba4406a4c0e26035ee4&scene=19#wechat_redirect');
+
 	
 
 	driver1.quit();
-
-	#driver.quit();
